e9d1b4fe9f3adcff35e288e3c6409317672bcd0a
Alan Gabriel <contatoalangabriel@gmail.com>
2021-03-26 18:16:39 -0300

    Use dynamic import to leaflet

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -1,15 +1,17 @@
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 
 const Map = () => (
-  <MapContainer center={[0, 0]} zoom={13} scrollWheelZoom={false}>
+  <MapContainer
+    center={[-15.504991, -45.396898]}
+    zoom={4.5}
+    style={{ height: '100%', width: '100%' }}
+  >
     <TileLayer
       attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
       url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
     />
-    <Marker position={[0, 0]}>
-      <Popup>
-        A pretty CSS3 popup. <br /> Easily customizable.
-      </Popup>
+    <Marker position={[-15.504991, -45.396898]}>
+      <Popup>Serra das Araras - MG</Popup>
     </Marker>
   </MapContainer>
 )
