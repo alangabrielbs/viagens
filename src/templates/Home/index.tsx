a70612755991cdaf948c083c08fdfddf0112d5e6
Alan Gabriel <contatoalangabriel@gmail.com>
2021-07-27 12:24:22 -0300

    remove firebase

--- src/templates/Home/index.tsx
+++ src/templates/Home/index.tsx
@@ -1,45 +1,14 @@
-import { useEffect } from 'react'
 import { NextSeo } from 'next-seo'
-
-import firebase from 'firebase'
-import 'firebase/messaging'
 
 import { InfoOutline } from '@styled-icons/evaicons-outline/InfoOutline'
 import dynamic from 'next/dynamic'
 
 import LinkWrapper from 'components/LinkWrapper'
 import { MapProps } from 'components/Map'
-import Firebase from 'services/firebase-init'
 
 const Map = dynamic(() => import('components/Map'), { ssr: false })
 
 export default function HomeTemplate({ places }: MapProps) {
-  const getMessage = async () => {
-    try {
-      const token = await Firebase.getInstance().init()
-
-      if (token) {
-        console.log('[TOKEN]:', token)
-
-        console.log('{message functions}')
-        const messaging = firebase.messaging()
-        messaging.onMessage((message) => console.log('foreground', message))
-      }
-    } catch (err) {
-      console.error(err)
-    }
-  }
-
-  useEffect(() => {
-    if ('serviceWorker' in navigator) {
-      console.log('[serviceWorker]')
-      navigator.serviceWorker.addEventListener('message', (event) =>
-        console.log('event for the service worker', event)
-      )
-    }
-    getMessage()
-  }, [])
-
   return (
     <>
       <NextSeo
